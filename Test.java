926840fb86d56c3e20e464348c74bb2b6fdf2fe2
Iskander Tabaev <tab_is@mail.ru>
2022-07-14 07:45:35 +0300

    V 1.5

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 public class Test  {
-    public static void main(String[] args) {
-        System.out.println("Hello world!123");
+    public static void main(String[] args) throws InterruptedException{
+        System.out.println("Hello world from Test.java");
+        int a=5;
+        while (a>0){
+            System.out.println(a);
+            
+            Thread.sleep(500);
+            a--;
+        }
+        System.out.println(GO!);
     }
 }
